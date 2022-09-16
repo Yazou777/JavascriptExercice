4017b28cf6057542d23046b707d9f17707db04dc
Kévin Wilga <wilga.kevin@sfr.fr>
2022-09-16 12:21:50 +0200

    inner

---
+++ asset/js/exfonction.js
@@ -0,0 +1,12 @@
+function produit(x, y)
+{
+var resultat = x * y
+return resultat;
+}
+produit(2, 5);
+console.log(produit(2,5));
+
+
+
+
+let html = document.getElementById("test").innerHTML = "<img src= https://ncode.amorce.org/ressources/Pool/TB_DEV/WEB_Javascript_BASES/images/papillon.jpg>"
