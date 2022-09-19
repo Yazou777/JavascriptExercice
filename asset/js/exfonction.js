e74cd439f3ef45f1463ee41044a5332fa982b133
Kévin Wilga <wilga.kevin@sfr.fr>
2022-09-19 16:48:00 +0200

    exercice tableau

--- asset/js/exfonction.js
+++ asset/js/exfonction.js
@@ -15,15 +15,14 @@ document.write("Le cube de "+nombre+" est égal à "+cube+"<br>Le produit de "+n
 /*var nombre = prompt("entrez un nombre");
 var multi = prompt("Entrez un multiplicateur");*/
 
-/*
+
 function produit(x ,y)
 { 
-    X=x;
-    Y=y;
-    cube = x*x*x; 
+    cube = x**3; 
 var resultat = x * y;
-console.log(egal);
-document.write("Le cube de "+X+" est égal à "+cube+"<br>Le produit de "+X+" x "+Y+" est égal à "+resultat);
+console.log(cube);
+console.log(resultat);
+document.write("Le cube de "+x+" est égal à "+cube+"<br>Le produit de "+x+" x "+y+" est égal à "+resultat);
 return  ;
 }
 var egal = produit(prompt("entrez un nombre") ,prompt("Entrez un multiplicateur"));
@@ -35,11 +34,14 @@ function image(image)
     document.getElementById("test").innerHTML = image;
 }
 image("<img src= https://ncode.amorce.org/ressources/Pool/TB_DEV/WEB_Javascript_BASES/images/papillon.jpg>");
-*/
 
+
+
+/*
 function strtok(str1, str2, n)
 {  
 const words = str1.split(str2);
 alert(words[n - 1]);
 }
 strtok("robert.dupont.amiens.8000",".",3 );
+*/
